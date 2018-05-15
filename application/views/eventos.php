3044008195cb1a2035413dc7f3042ec82703b480
matheusXAlves <matheusexemple@hotmail.com>
2018-05-15 14:31:05 -0300

    Correções de CSS.

--- application/views/eventos.php
+++ application/views/eventos.php
@@ -1,9 +1,11 @@
 	<div class="container-fluid bg-2 text-center">
 		<h2>Encontre Eventos Para Participar</h2>
 	</div>
-	<a href="#" class="Botão4 btn btn-default btn-lg"> <strong>Criar um Evento</strong>
-		<span class="glyphicon glyphicon-pencil"></span>
-	</a>
+	<div class="container-fluid text-center">
+		<a href="#" class="Botão4 btn btn-default btn-lg"> <strong>Criar um Evento</strong>
+			<span class="glyphicon glyphicon-pencil"></span>
+		</a>
+	</div>
 	<div class="container-fluid bg-3 text-center">
 			<h3>EVENTOS</h3>
 		 <div class="row">
@@ -12,10 +14,12 @@
 				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
 				<a href="#"> 
 					<img src="Cac4.png" alt="Image" width="350" height="500">
-				</a> 
-				<a href="#" class="Botão1 btn btn-success btn-lg">
-					<span class="glyphicon glyphicon-ok"></span> Participar 
 				</a>
+				<div class="container-fluid text-center">
+					<a href="#" class="Botão1 btn btn-success btn-lg">
+						<span class="glyphicon glyphicon-ok"></span> Participar 
+					</a>
+				</div>
 			</div>
 			<div class="col-sm-4">
 				<h3>Evento 2</h3>
@@ -23,9 +27,11 @@
 				<a href="#"> 
 					<img src="Cac4.png" alt="Image" width="350" height="500">
 				</a>
-				<a href="#" class="Botão2 btn btn-success btn-lg">
-					<span class="glyphicon glyphicon-ok"></span> Participar 
-				</a>			
+				<div class="container-fluid text-center">
+					<a href="#" class="Botão2 btn btn-success btn-lg">
+						<span class="glyphicon glyphicon-ok"></span> Participar 
+					</a>
+				</div>
 			</div>
 			<div class="col-sm-4">
 				<h3>Evento 3</h3>
@@ -33,12 +39,16 @@
 				<a href="#"> 
 					<img src="Cac4.png" alt="Image" width="350" height="500">
 				</a>
-				<a href="#" class="Botão3 btn btn-success btn-lg">
-					<span class="glyphicon glyphicon-ok"></span> Participar 
-				</a>			
+				<div class="container-fluid text-center">
+					<a href="#" class="Botão3 btn btn-success btn-lg">
+						<span class="glyphicon glyphicon-ok"></span> Participar 
+					</a>
+				</div>
 			</div>
 		</div>
 	</div>
-	<a href="#" class="Botão btn btn-info btn-lg"> Próxima Página
-		<span class="glyphicon glyphicon-arrow-right"></span>
-	</a>
+	<div class="container-fluid text-center">
+		<a href="#" class="Botão btn btn-info btn-lg"> Próxima Página
+			<span class="glyphicon glyphicon-arrow-right"></span>
+		</a>
+	</div>
