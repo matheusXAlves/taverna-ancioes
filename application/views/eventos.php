632c069af4717618ecc3f93d4c197994793cac2a
matheusXAlves <matheusexemple@hotmail.com>
2018-05-15 15:51:59 -0300

    Melhorando o visual das páginas de eventos e de grupos.

--- application/views/eventos.php
+++ application/views/eventos.php
@@ -1,53 +1,119 @@
-	<div class="container-fluid bg-2 text-center">
-		<h2>Encontre Eventos Para Participar</h2>
-	</div>
-	<div class="container-fluid text-center">
-		<a href="#" class="Botão4 btn btn-default btn-lg"> <strong>Criar um Evento</strong>
+	<div class="container-fluid bg-3 text-center">
+		<div class="container-fluid bg-4 text-center">
+			<h2>Encontre Eventos Para Participar</h2>
+			<h3>ou</h3>
+			<a href="#" class="Botão4 btn btn-default btn-lg">
+				<strong>Crie um Evento</strong>
 			<span class="glyphicon glyphicon-pencil"></span>
 		</a>
-	</div>
-	<div class="container-fluid bg-3 text-center">
-			<h3>EVENTOS</h3>
-		 <div class="row">
-			<div class="col-sm-4">
-				<a href="#"> 
-			    	<img src="https://i0.wp.com/www.wizards.com/dnd/images/Tyranny_Wallpaper.jpg" class="img-rounded" alt="Image" width="400" height="300">
-				</a> 
-				<h3>Evento 1</h3>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-				<div class="container-fluid text-center">
-					<a href="#" class="Botão1 btn btn-success btn-lg">
-						<span class="glyphicon glyphicon-ok"></span> Participar 
-					</a>
+	   	</div>
+
+		<br>
+		<div class="row">
+			<div class="col-md-4">
+				<div class="card text-center">
+					<img src="https://dnd35.files.wordpress.com/2013/08/thelastthreshold_2560x1600_wallpaper.jpg?w=683&h=426" class="img-rounded img-responsive img-thumbnail" alt="Image" width="400" height="300">
+					<h3 class="card-header">
+						Evento 1
+					</h3>
+					<div class="card-body">
+						<p class="card-text">
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+						</p>
+						<a href="#" class="Botão3 btn btn-success btn-lg">
+							<span class="glyphicon glyphicon-ok"></span> Participar 
+						</a>
+					</div>
 				</div>
 			</div>
-			<div class="col-sm-4">
-				<a href="#"> 
-			    	<img src="https://i.ytimg.com/vi/qJzEXAwBhFw/maxresdefault.jpg" class="img-rounded" alt="Image" width="400" height="300">
-				</a>
-				<h3>Evento 2</h3>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-				
-				<div class="container-fluid text-center">
-					<a href="#" class="Botão2 btn btn-success btn-lg">
-						<span class="glyphicon glyphicon-ok"></span> Participar 
-					</a>
+			<div class="col-md-4">
+				<div class="card text-center">
+					<img src="https://i.ytimg.com/vi/qJzEXAwBhFw/maxresdefault.jpg" class="img-rounded img-responsive img-thumbnail" alt="Image" width="400" height="300">
+					<h3 class="card-header">
+						Evento 2
+					</h3>
+					<div class="card-body">
+						<p class="card-text">
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+						</p>
+						<a href="#" class="Botão3 btn btn-success btn-lg">
+							<span class="glyphicon glyphicon-ok"></span> Participar 
+						</a>
+					</div>
 				</div>
 			</div>
-			<div class="col-sm-4">
-				<a href="#"> 
-			    	<img src="https://dnd35.files.wordpress.com/2013/08/thelastthreshold_2560x1600_wallpaper.jpg?w=683&h=426" class="img-rounded" alt="Image" width="400" height="300">
-				</a>
-				<h3>Evento 3</h3>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-				<div class="container-fluid text-center">
-					<a href="#" class="Botão3 btn btn-success btn-lg">
-						<span class="glyphicon glyphicon-ok"></span> Participar 
-					</a>
+			<div class="col-md-4">
+				<div class="card text-center">
+					<img src="https://i0.wp.com/www.wizards.com/dnd/images/Tyranny_Wallpaper.jpg" class="img-rounded img-responsive img-thumbnail" alt="Image" width="400" height="300">
+					<h3 class="card-header">
+						Evento 3
+					</h3>
+					<div class="card-body">
+						<p class="card-text">
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+						</p>
+						<a href="#" class="Botão3 btn btn-success btn-lg">
+							<span class="glyphicon glyphicon-ok"></span> Participar 
+						</a>
+					</div>
 				</div>
 			</div>
 		</div>
+		
+		<br><br><br>
+		<div class="row">
+			<div class="col-md-4">
+				<div class="card text-center">
+					<img src="https://dnd35.files.wordpress.com/2013/08/thelastthreshold_2560x1600_wallpaper.jpg?w=683&h=426" class="img-rounded img-responsive img-thumbnail" alt="Image" width="400" height="300">
+					<h3 class="card-header">
+						Evento 4
+					</h3>
+					<div class="card-body">
+						<p class="card-text">
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+						</p>
+						<a href="#" class="Botão3 btn btn-success btn-lg">
+							<span class="glyphicon glyphicon-ok"></span> Participar 
+						</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="card text-center">
+					<img src="https://i.ytimg.com/vi/qJzEXAwBhFw/maxresdefault.jpg" class="img-rounded img-responsive img-thumbnail" alt="Image" width="400" height="300">
+					<h3 class="card-header">
+						Evento 5
+					</h3>
+					<div class="card-body">
+						<p class="card-text">
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+						</p>
+						<a href="#" class="Botão3 btn btn-success btn-lg">
+							<span class="glyphicon glyphicon-ok"></span> Participar 
+						</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="card text-center">
+					<img src="https://i0.wp.com/www.wizards.com/dnd/images/Tyranny_Wallpaper.jpg" class="img-rounded img-responsive img-thumbnail" alt="Image" width="400" height="300">
+					<h3 class="card-header">
+						Evento 6
+					</h3>
+					<div class="card-body">
+						<p class="card-text">
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+						</p>
+						<a href="#" class="Botão3 btn btn-success btn-lg">
+							<span class="glyphicon glyphicon-ok"></span> Participar 
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+
 	</div>
+
 	<div class="container-fluid text-center">
 		<a href="#" class="Botão btn btn-info btn-lg"> Próxima Página
 			<span class="glyphicon glyphicon-arrow-right"></span>
--- application/views/grupos.php
+++ application/views/grupos.php
@@ -1,80 +1,124 @@
-	<div class="container-fluid bg-4 text-center">
-     	<h2>Encontre Grupos para Entrar</h2>
-   	</div>
-   	<div class="container-fluid bg-2 text-center">
-     	<h3>Grupos</h3>
-   	</div>
-     	
-	<div class="container-fluid text-center">
-		<a href="#" class="Botão4 btn btn-default btn-lg"> 
-     		<strong>Criar um Grupo</strong>
+   	<div class="container-fluid bg-3 text-center">
+		<div class="container-fluid bg-4 text-center">
+	     	<h2>Encontre Grupos para Entrar</h2>
+	     	<h3>ou</h3>
+	     	<a href="#" class="Botão4 btn btn-default btn-lg"> 
+     			<strong>Crie um Grupo</strong>
 	        <span class="glyphicon glyphicon-pencil"></span>
 	    </a>
-	</div>
+	   	</div>
 
-   	<div class="container-fluid bg-3 text-center">
+		<br>
 		<div class="row">
-			<div class="col-md-12">
-				<div class="row">
-					<div class="col-md-4">
-						<div class="card">
-							<img src="<?= base_url('assets/img/d20.png') ?>" class= "img-responsive " style="display:inline" alt="Image" width="250" height="150">
-							<div class="card-block">
-								<h5 class="card-title">
-									Grupo 1
-								</h5>
-								<p class="card-text">
-									Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
-								</p>
-								<p>Eventos Ocorridos: 50</p>
-								<p>Proximo evento: Sábado, Dia 15 de junho</p>
-								<p>
-									<a class="btn btn-success" href="#">
-									<span class="glyphicon glyphicon-ok"></span> Entrar
-									</a>
-								</p>
-							</div>
-						</div>
-					</div>
-					<div class="col-md-4">
-						<div class="card">
-							<img src="Cac4.png" class= "img-responsive " style="display:inline" alt="Image" width="250" height="150">
-							<div class="card-block">
-								<h5 class="card-title">
-									Grupo 2
-								</h5>
-								<p class="card-text">
-									Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
-								</p>
-								<p>Eventos Ocorridos: 04</p>
-								<p>Proximo evento: Sábado, Dia 01 de dezembro</p>
-								<p>
-									<a class="btn btn-success" href="#">
-									<span class="glyphicon glyphicon-ok"></span> Entrar
-									</a>
-								</p>
-							</div>
-						</div>
-					</div>
-					<div class="col-md-4">
-						<div class="card">
-							<img src="Cac4.png" class= "img-responsive" style="display:inline" alt="Image" width="250" height="150">
-							<div class="card-block">
-								<h5 class="card-title">
-									Grupo 3
-								</h5>
-								<p class="card-text">
-									Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
-								</p>
-								<p>Eventos Ocorridos: 32</p>
+			<div class="col-md-4">
+				<div class="card text-center">
+					<img src="https://dnd35.files.wordpress.com/2013/08/thelastthreshold_2560x1600_wallpaper.jpg?w=683&h=426" class="img-rounded img-responsive img-thumbnail" alt="Image" width="400" height="300">
+					<h3 class="card-header">
+						Grupo 1
+					</h3>
+					<div class="card-body">
+						<p class="card-text">
+							Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+						</p>
+						<p>Eventos Ocorridos: 32</p>
 								<p>Proximo evento: Sábado, Dia 21 de setembro</p>
-								<p>
-									<a class="btn btn-success" href="#">
-									<span class="glyphicon glyphicon-ok"></span> Entrar
-									</a>
-								</p>
-							</div>
-						</div>
+						<a href="#" class="Botão3 btn btn-success btn-lg">
+							<span class="glyphicon glyphicon-ok"></span> Entrar 
+						</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="card text-center">
+					<img src="https://i.ytimg.com/vi/qJzEXAwBhFw/maxresdefault.jpg" class="img-rounded img-responsive img-thumbnail" alt="Image" width="400" height="300">
+					<h3 class="card-header">
+						Grupo 2
+					</h3>
+					<div class="card-body">
+						<p class="card-text">
+							Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+						</p>
+						<p>Eventos Ocorridos: 32</p>
+								<p>Proximo evento: Sábado, Dia 21 de setembro</p>
+						<a href="#" class="Botão3 btn btn-success btn-lg">
+							<span class="glyphicon glyphicon-ok"></span> Entrar 
+						</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="card text-center">
+					<img src="https://i0.wp.com/www.wizards.com/dnd/images/Tyranny_Wallpaper.jpg" class="img-rounded img-responsive img-thumbnail" alt="Image" width="400" height="300">
+					<h3 class="card-header">
+						Grupo 3
+					</h3>
+					<div class="card-body">
+						<p class="card-text">
+							Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+						</p>
+						<p>Eventos Ocorridos: 32</p>
+								<p>Proximo evento: Sábado, Dia 21 de setembro</p>
+						<a href="#" class="Botão3 btn btn-success btn-lg">
+							<span class="glyphicon glyphicon-ok"></span> Entrar 
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		<br><br><br>
+		<div class="row">
+			<div class="col-md-4">
+				<div class="card text-center">
+					<img src="https://dnd35.files.wordpress.com/2013/08/thelastthreshold_2560x1600_wallpaper.jpg?w=683&h=426" class="img-rounded img-responsive img-thumbnail" alt="Image" width="400" height="300">
+					<h3 class="card-header">
+						Grupo 4
+					</h3>
+					<div class="card-body">
+						<p class="card-text">
+							Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+						</p>
+						<p>Eventos Ocorridos: 32</p>
+								<p>Proximo evento: Sábado, Dia 21 de setembro</p>
+						<a href="#" class="Botão3 btn btn-success btn-lg">
+							<span class="glyphicon glyphicon-ok"></span> Entrar 
+						</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="card text-center">
+					<img src="https://i.ytimg.com/vi/qJzEXAwBhFw/maxresdefault.jpg" class="img-rounded img-responsive img-thumbnail" alt="Image" width="400" height="300">
+					<h3 class="card-header">
+						Grupo 5
+					</h3>
+					<div class="card-body">
+						<p class="card-text">
+							Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+						</p>
+						<p>Eventos Ocorridos: 32</p>
+								<p>Proximo evento: Sábado, Dia 21 de setembro</p>
+						<a href="#" class="Botão3 btn btn-success btn-lg">
+							<span class="glyphicon glyphicon-ok"></span> Entrar 
+						</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="card text-center">
+					<img src="https://i0.wp.com/www.wizards.com/dnd/images/Tyranny_Wallpaper.jpg" class="img-rounded img-responsive img-thumbnail" alt="Image" width="400" height="300">
+					<h3 class="card-header">
+						Grupo 6
+					</h3>
+					<div class="card-body">
+						<p class="card-text">
+							Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+						</p>
+						<p>Eventos Ocorridos: 32</p>
+								<p>Proximo evento: Sábado, Dia 21 de setembro</p>
+						<a href="#" class="Botão3 btn btn-success btn-lg">
+							<span class="glyphicon glyphicon-ok"></span> Entrar 
+						</a>
 					</div>
 				</div>
 			</div>
